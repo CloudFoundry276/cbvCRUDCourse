becf89f9cd6dd776be07f5bb1983323681ea558f
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-21 21:53:01 +0530

    class based views course crud app project is done.

--- cbvCRUDCourseApp/views.py
+++ cbvCRUDCourseApp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from cbvCRUDCourseApp.models import Course
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class CourseListView(ListView):
+    model = Course
+
+
+class CourseDetailView(DetailView):
+    model = Course
+
+
+class CourseCreateView(CreateView):
+    model = Course
+    fields = ('name', 'description', 'instructor', 'rating')
+
+
+class CourseUpdateView(UpdateView):
+    model = Course
+    fields = ('description', 'instructor', 'rating')
+
+
+class CourseDeleteView(DeleteView):
+    model = Course
+    success_url = reverse_lazy('courses')
